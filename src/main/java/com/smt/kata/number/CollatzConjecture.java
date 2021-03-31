1739bbbece9cafc68064d133b64b856a02a45e21
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2021-03-31 11:03:26 -0600

    collatzConjecture

--- src/main/java/com/smt/kata/number/CollatzConjecture.java
+++ src/main/java/com/smt/kata/number/CollatzConjecture.java
@@ -54,6 +54,16 @@ public class CollatzConjecture {
 	 * @return
 	 */
 	public int calculate(int n) {
-		return n;
+		if(n < 0) n = n * -1;
+		int count = 0;
+		while(n > 1) {
+			if (n%2 == 0) {
+				n = n/2;
+			}else {
+				n = n*3 +1;
+			}
+			count ++;
+		}
+		return count;
 	}
 }

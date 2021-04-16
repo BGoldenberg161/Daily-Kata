ab14e34343d328cfd038d07e016c3bf30efc4a49
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2021-04-16 09:37:16 -0600

    baseConverter

--- src/main/java/com/smt/kata/math/BaseConverter.java
+++ src/main/java/com/smt/kata/math/BaseConverter.java
@@ -21,51 +21,88 @@ package com.smt.kata.math;
  ****************************************************************************/
 public class BaseConverter {
 
-	/**
-	 * Converts a decimal value to its binary equivalent
-	 * @param value Decimal value to be converted
-	 * @return binary number as an integer
-	 */
-	public int decimaltoBinary(int value) {
-		return Integer.valueOf(convertToBase(value, 2));
-	}
-	
-	/**
-	 * Converts a decimal value to its octal equivalent
-	 * @param value Decimal value to be converted
-	 * @return Octal number as an integer
-	 */
-	public int decimaltoOctal(int value) {
-		return Integer.valueOf(convertToBase(value, 8));
-	}
+    /**
+     * Converts a decimal value to its binary equivalent
+     * @param value Decimal value to be converted
+     * @return binary number as an integer
+     */
+    public int decimaltoBinary(int value) {
+        return Integer.valueOf(convertToBase(value, 2));
+    }
+    // div by 2
+    // Get the integer quotient for the next iteration.
+    // Get the remainder for the binary digit.
+    // Repeat the steps until the quotient is equal to 0.
 
-	/**
-	 * Converts a decimal value to its hexidecimal equivalent
-	 * @param value Decimal value to be converted
-	 * @return Hexideciaml number as a string
-	 * 
-	 */
-	public String decimaltoHexidecimal(int value) {
-		return convertToBase(value, 16);
-	}
-	
-	/**
-	 * Converts a decimal value into a binary, octal or hex value
-	 * @param value Decimal value to convert
-	 * @param base Base 2, 8 or 16
-	 * @return String of the converted value.  A string is used as hex #s have characters
-	 */
-	private String convertToBase(int value, int base) {
-		return value + base + "";
-	}
-	
-	/**
-	 * Converts a value with the given base back to decimal
-	 * @param value Value to be converted
-	 * @param base Base 2, 8 or 16
-	 * @return Converted number in decimal (base10) format
-	 */
-	public int convertToDecimal(String value, int base) {
-		return base;
-	}
+    /**
+     * Converts a decimal value to its octal equivalent
+     * @param value Decimal value to be converted
+     * @return Octal number as an integer
+     */
+    public int decimaltoOctal(int value) {
+        return Integer.valueOf(convertToBase(value, 8));
+    }
+    // div by 8
+
+    /**
+     * Converts a decimal value to its hexidecimal equivalent
+     * @param value Decimal value to be converted
+     * @return Hexideciaml number as a string
+     * 
+     */
+    public String decimaltoHexidecimal(int value) {
+        return convertToBase(value, 16);
+    }
+    
+    /**
+     * Converts a decimal value into a binary, octal or hex value
+     * @param value Decimal value to convert
+     * @param base Base 2, 8 or 16
+     * @return String of the converted value.  A string is used as hex #s have characters
+     */
+    private String convertToBase(int value, int base) {
+        // Divide value by base
+        // Remainder becomes rightmost digit
+        // Loop with new value
+        String output = "";
+        int currentVal = value;
+        while(currentVal > 0){
+            int newValue = currentVal / base;
+            int remainder = currentVal % base;
+            System.out.println("Current: " + currentVal + " NewValue:" + newValue + " Remainder:" + remainder);
+
+            if(currentVal == base){
+                output = "0" + output;
+            }else{
+                output = remainder + output;
+            }
+            currentVal = newValue;
+            if(remainder == newValue){
+                output = remainder + output;
+                break;
+            }
+        }
+        //divide by new base, record remainder
+        return output;
+    }
+    
+    /**
+     * Converts a value with the given base back to decimal
+     * @param value Value to be converted
+     * @param base Base 2, 8 or 16
+     * @return Converted number in decimal (base10) format
+     */
+    public int convertToDecimal(String value, int base) {
+        int dec = 0;
+        int index = 0;
+        for(int x = value.length() - 1; x >= 0; x--){
+            int digit = value.charAt(x)-'0';
+            double num = digit * (Math.pow((double)base, (double)index));
+            System.out.println(index + " " + num);
+            dec += num;
+            index++;
+        }
+
+        return dec;
+    }
 }

f86ea1b0e4a2183f84565d690605559a77b87cf1
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2021-04-20 10:22:03 -0600

    CharacterBoard

--- src/main/java/com/smt/kata/data/CharacterBoard.java
+++ src/main/java/com/smt/kata/data/CharacterBoard.java
@@ -1,5 +1,8 @@
 package com.smt.kata.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /****************************************************************************
  * <b>Title:</b> CharacterBoard.java
  * <b>Project:</b> SMT-Kata
@@ -32,23 +35,80 @@ package com.smt.kata.data;
  * 
  ****************************************************************************/
 public class CharacterBoard {
-	
-	protected char[][] board;
-	
-	/**
-	 * Initializes the class with a 2d matrix
-	 * @param board matrix of characters
-	 */
-	public CharacterBoard(char[][] board) {
-		this.board = board;
-	}
+    
+    protected char[][] board;
+    
+    /**
+     * Initializes the class with a 2d matrix
+     * @param board matrix of characters
+     */
+    public CharacterBoard(char[][] board) {
+        this.board = board;
+    }
 
-	/**
-	 * Runs through the character board and finds the provided word
-	 * @param word Word to search the board
-	 * @return True if word is in the board, false otherwise
-	 */
-	public boolean exists(String word) {
-		return word == null;
-	}
+    /**
+     * Runs through the character board and finds the provided word
+     * @param word Word to search the board
+     * @return True if word is in the board, false otherwise
+     */
+    public boolean exists(String word) {
+        if(word == null || word.length() == 0)
+            return false;
+        
+        word = word.toUpperCase();
+        char first = word.charAt(0);
+        
+        /** Loop to find good entry point **/
+        for(int row = 0; row < board.length; row++) {
+            for(int col = 0; col < board[0].length; col++) {
+                if(board[row][col] == first && pathfind(word.substring(1), row, col)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean pathfind(String word, int row, int col) {
+        if(word.length() == 0) return true;
+        List<CharIndex> nextChars = getNextChars(row,col);
+        
+        /** Loop over next characters **/
+        for(CharIndex c : nextChars) {
+            if(c.character == word.charAt(0) && pathfind(word.substring(1), c.row, c.col)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public List<CharIndex> getNextChars(int row, int col) {
+        List<CharIndex> result = new ArrayList<>();
+        //top
+        if (row > 0)
+            result.add(new CharIndex(this.board[row - 1][col], row - 1, col));
+        // left
+        if (col > 0)
+            result.add(new CharIndex(this.board[row][col - 1], row, col - 1));    
+        // bottom
+        if (row < board.length - 1)
+            result.add(new CharIndex(this.board[row + 1][col], row + 1, col));
+        // right
+        if (col < board[0].length - 1)
+            result.add(new CharIndex(this.board[row][col + 1], row, col + 1));
+        
+        return result;
+    }
+}
+
+class CharIndex {
+    public char character;
+    public int row;
+    public int col;
+    
+    public CharIndex(char character, int row, int col) {
+        this.character = character;
+        this.row = row;
+        this.col = col;
+    }
 }
